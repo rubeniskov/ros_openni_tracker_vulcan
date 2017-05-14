4f9bc5c02786a869a03e2b033836b46711f96e9b
rubeniskov <me@rubeniskov.com>
2017-05-15 00:07:36 +0200

    update: add stepper X Axis handler

--- arduino/openni_tracker_vulcanuino/openni_tracker_vulcanuino.ino
+++ arduino/openni_tracker_vulcanuino/openni_tracker_vulcanuino.ino
@@ -18,21 +18,39 @@
   #include <WProgram.h>
 #endif
 
-#include <Servo.h> 
+#include <AccelStepper.h>
 #include <ros.h>
 #include <geometry_msgs/Point32.h>
 
 ros::NodeHandle  nh;
 
-unsigned long lastDetection;
+unsigned long lastDetection = millis();
 const unsigned long TIME_TO_IDLE = 5000;
 
-Servo servoX, servoY;
+AccelStepper XAxisStepper(1,4,5);
+
+
+void initAxis(){
+   XAxisStepper.setAcceleration(1000);
+   XAxisStepper.setMaxSpeed(700);
+}
+void moveXAxis(int degrees){
+    if(XAxisStepper.distanceToGo() == 0){
+      XAxisStepper.moveTo(360 - (3454 / 360) * degrees);
+  }
+  
+}
+
+void moveTo(int degreesX, int degreesY) {
+   moveXAxis(degreesX);
+}
+
 
 void servo_cb( const geometry_msgs::Point32& cmd_msg){
   lastDetection = millis();
-  servoX.write(cmd_msg.x * 180); //set servo angle, should be from 0-180
-  servoY.write(cmd_msg.y * 180); //set servo angle, should be from 0-180  
+  moveTo(cmd_msg.x * 60, 0);
+  //servoX.write(cmd_msg.x * 180); //set servo angle, should be from 0-180
+  //servoY.write(cmd_msg.y * 180); //set servo angle, should be from 0-180  
   digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
 }
 
@@ -40,23 +58,27 @@ void servo_cb( const geometry_msgs::Point32& cmd_msg){
 ros::Subscriber<geometry_msgs::Point32> sub("vulcanuino", servo_cb);
 
 void resetPosition(){
-  servoX.write(0);
-  servoY.write(0);
+  //servoX.write(0);
+  //servoY.write(0);
 }
 
 void setup(){
   pinMode(13, OUTPUT);
 
+  initAxis();
   nh.initNode();
   nh.subscribe(sub);
-  servoX.attach(9); //attach it to pin 9
-  servoY.attach(10); //attach it to pin 10
+  //servoX.attach(9); //attach it to pin 9
+  //servoY.attach(10); //attach it to pin 10
+  
   
 }
-
+bool dir = false;
 void loop(){
   nh.spinOnce();
   if(millis() - lastDetection > TIME_TO_IDLE)
-      resetPosition();
-  delay(1);
+      moveTo(0, 0);
+      
+  //moveTo(360, 0);
+  XAxisStepper.run();
 }
